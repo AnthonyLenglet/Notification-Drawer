2764b4f25c78831825f211ba45da496a4f7320b8
AnthonyLenglet <Anthony.Lenglet@hotmail.com>
2018-04-06 19:24:47 +0200

    made the library work with import

--- notificationDrawer.js
+++ notificationDrawer.js
@@ -11,7 +11,7 @@
 /* );                                                                                                   */
 /*                                                                                                      */
 /* notif.sendNotification(                                                                              */
-/*   notification type ('error', 'confirm', 'info', 'warning'),                                         */
+/*   notification type ('error', 'success', 'info', 'warning'),                                         */
 /*   {                                                                                                  */
 /*     title: 'Notification Title',                                                                     */
 /*     body: 'Notification body'                                                                        */
@@ -19,8 +19,9 @@
 /*   time on screen (ms)                                                                                */
 /* );                                                                                                   */
 
+import notificationItem from './notificationItem'
 
-class notification {
+export default class notification {
     //initiate the notification drawer
     constructor(direction, drawerName) {
         this.ownerDrawer = drawerName;
@@ -28,111 +29,115 @@ class notification {
         this.drawerDirection = direction;
         this.drawerPadding = 15;
         this.spaceBetweenNotifs = 15;
-        
+
         //this list will contain all the notification item objects in this drawer
         this.notifObjList = [];
-        
+        this.notificationCount = 0;
+
         this.notifDrawer = document.createElement("div");
         this.notifDrawer.classList.add(this.notificationDrawerName);
         document.body.appendChild(this.notifDrawer);
         this.drawerSelector = '.'+this.notificationDrawerName;
-        
+
+        //create css stylesheet to modify
+        var styleEl = document.createElement('style');
+        document.head.appendChild(styleEl);
+        this.styleSheet = styleEl.sheet;
+
         //position the drawer according to the given placement
         var beginningCSS = "."+this.notificationDrawerName+" { position: fixed;";
         switch(this.drawerDirection) {
             case 'topleft':
-                document.styleSheets[0].insertRule(beginningCSS+" top: "+this.drawerPadding+"px; left: "+this.drawerPadding+"px; }", 1);
+                this.styleSheet.insertRule(beginningCSS+" top: "+this.drawerPadding+"px; left: "+this.drawerPadding+"px; }", 0);
                 break;
             case 'topright':
-                document.styleSheets[0].insertRule(beginningCSS+" top: "+this.drawerPadding+"px; right: "+this.drawerPadding+"px; }", 1);
+                this.styleSheet.insertRule(beginningCSS+" top: "+this.drawerPadding+"px; right: "+this.drawerPadding+"px; }", 0);
                 break;
             case 'bottomleft':
-                document.styleSheets[0].insertRule(beginningCSS+" bottom: "+this.drawerPadding+"px; left: "+this.drawerPadding+"px; }", 1);
+                this.styleSheet.insertRule(beginningCSS+" bottom: "+this.drawerPadding+"px; left: "+this.drawerPadding+"px; }", 0);
                 break;
             case 'bottomright':
-                document.styleSheets[0].insertRule(beginningCSS+" bottom: "+this.drawerPadding+"px; right: "+this.drawerPadding+"px; }", 1);
+                this.styleSheet.insertRule(beginningCSS+" bottom: "+this.drawerPadding+"px; right: "+this.drawerPadding+"px; }", 0);
                 break;
         }
     }
-    
+
     //create the notification item
     sendNotification(type, content, timer) {
         var p = this;
-        
+
         //create a new notificationitem object and push it to the notification list
-        var notifObject = new notificationItem(p.drawerDirection, p.ownerDrawer, p.drawerPadding, timer);
+        var notifObject = new notificationItem(p.drawerDirection, p.ownerDrawer, p.drawerPadding, p.styleSheet, timer);
         this.notificationCount++;
         notifObject.notifID = this.notificationCount;
+
         this.notifObjList.push(notifObject);
-        
+
         //create and append the displayed notification
         content.owner = p.ownerDrawer;
         document.querySelector(this.drawerSelector).append(notifObject.create(type, content));
-        
+
         //slide the notification into view
         this.pushNotifOnScreen(notifObject.notifHTML);
-        
+
         //add notification events on mouseclick
         var mousedownHandler = function(e){
             e.preventDefault();
             e.target.style.transform = 'scale(0.95)';
         };
-        
+
         var mouseupHandler = function(e) {
             //Close the notification
             e.preventDefault();
-            
+
             //Before anything, make sure to disable the timeout
             clearTimeout(notifObject.TimeoutFunc);
-            
+
             e.target.style.transform = 'scale(1)';
             var drawerlist = p.notifDrawer;
 
-            //get index of clicked notification
-            var child = e.target;
-            var i = 0;
-            while( (child = child.previousSibling) != null ) 
-              i++;
-            
-            if(i>0){p.regroupNotif(i);}
-            
-            p.notifObjList.splice(i, 1);
+            p.regroupNotif(notifObject.notifID-1);
+
+            p.notifObjList.splice(notifObject.notifID-1, 1);
             notifObject.destroy(p.drawerDirection);
+            p.notificationCount--;
         };
-        
+
         var mouseoutHandler = function(e){
             e.preventDefault();
             e.target.style.transform = 'scale(1)';
         };
-        
+
         notifObject.notifHTML.addEventListener("transitionend", function(e){
             notifObject.notifHTML.addEventListener("mousedown", mousedownHandler);
             notifObject.notifHTML.addEventListener("mouseup", mouseupHandler);
             notifObject.notifHTML.addEventListener("mouseout", mouseoutHandler);
         });
-        
-        //Add timeout to automaitcaly dispose of the notification
+
+        //Add timeout to automaticaly dispose of the notification
         notifObject.TimeoutFunc = setTimeout(
             function(){
                 //get index of clicked notification
                 var child = notifObject.notifHTML;
+
                 var i = 0;
-                while( (child = child.previousSibling) != null ) 
+                while( (child = child.previousSibling) != null )
                   i++;
 
                 if(i>0){p.regroupNotif(i);}
-                
+
                 p.notifObjList.splice(i, 1);
                 notifObject.destroy(p.drawerDirection);
+                p.notificationCount--;
             },
             notifObject.notifTimer
         );
     }
-    
+
     //push the notification item on screen
     pushNotifOnScreen(notif) {
         var p = this;
-        
+
         //push every possible previous notifications down or up to make space for the new one
         for(var i=p.notifObjList.length-2;i>=0;i--){
             switch(p.drawerDirection){
@@ -146,9 +151,31 @@ class notification {
                     break;
             }
         }
-        
+
         //slide the new notification into view
-        window.requestAnimationFrame(function(){
+
+        // BUG: Note pour les future mainteneur / dev de Seprote 3
+        // Celui la est bizzare, donc je passe en francais pour
+        // que ce soit plus facile a comprendre.
+        //
+        // Note: Le timeout est utilisé pour contourner le problème
+        // pour le moment, mais une correction définitive serait mieux.
+        //
+        // Recréation: Sur la page login, appuyer sur le bouton
+        // de connexion.
+        //
+        // Resultat: Une notification apparait sans animation, et
+        // les actions de clique ne marchent pas.
+        //
+        // Cause: L'élément DOM crée en javascript n'est pas encore
+        // entièrement chargé avant l'execution des fonctions
+        // ajoutant les actions et créant l'animation.
+        //
+        // Solution: Refaire cette partie du code pour faire en
+        // sorte qu'elle ne s'execute que lorsque l'élement DOM est
+        // entièrement chargé.
+
+        setTimeout(()=>{
             switch(p.drawerDirection) {
                 case 'topleft':
                 case 'bottomleft':
@@ -159,9 +186,12 @@ class notification {
                     notif.style.right = '15px';
                     break;
             }
-        });
+        }, 10);
+
+        // /BUG
     }
-    
+
+
     //Regroup all the notifications together by moving the
     //ones after the newly deleted notification up or down
     regroupNotif(index) {
--- notificationItem.js
+++ notificationItem.js
@@ -11,7 +11,7 @@
 /* );                                                                                                   */
 /*                                                                                                      */
 /* notif.sendNotification(                                                                              */
-/*   notification type ('error', 'confirm', 'info', 'warning'),                                         */
+/*   notification type ('error', 'success', 'info', 'warning'),                                         */
 /*   {                                                                                                  */
 /*     title: 'Notification Title',                                                                     */
 /*     body: 'Notification body'                                                                        */
@@ -19,27 +19,30 @@
 /*   time on screen (ms)                                                                                */
 /* );                                                                                                   */
 
+import './strcast.min'
 
-class notificationItem {
-    constructor(direction, owner, padding, timer) {
+export default class notificationItem {
+    constructor(direction, owner, padding, css, timer) {
         this.owner = owner;
-        
+
         this.colorError = '#ff3014';
-        this.colorConfirm = '#7be016';
+        this.colorSuccess = '#7be016';
         this.colorWarning = '#f8db39';
         this.colorInfo = '#a2ffe4';
-        
+
         this.opacity = '0.9';
-        
+
         this.notifWidth = 350;
         this.notifHeight = 125;
         this.itemPadding = padding;
-        
+
+        this.style = css;
+
         this.notifTimer = timer;
         this.TimeoutFunc;
-        
-        this.notificationHTML = "<div class='@@owner@@-notification-item'><div class='notification-content'><p class='notification-title'>@@title@@</p><p class='notification-body'>@@body@@</p></div></div>";
-        
+
+        this.notificationHTML = "<div class='..owner..-notification-item'><div class='notification-content'><p class='notification-title'>..title..</p><p class='notification-body'>..body..</p></div></div>";
+
         var cssItemPos = '';
         switch(direction) {
             case 'topleft':
@@ -55,53 +58,53 @@ class notificationItem {
                 cssItemPos = 'right: -'+this.notifWidth+'px; bottom: '+this.itemPadding+'px;';
                 break;
         }
-        
-        document.styleSheets[0].insertRule("."+this.owner+"-notification-item { position: inherit; width: "+this.notifWidth+"px; height: "+this.notifHeight+"px; "+cssItemPos+" overflow: hidden; cursor: pointer; transition: transform 0.25s ease-out, right 0.25s ease-out, left 0.25s ease-out, top 0.25s ease-out, bottom 0.25s ease-out; }", 1);
-        
-        document.styleSheets[0].insertRule(".notification-content { padding: 15px 0 0 15px; pointer-events: none; }", 1);
-        
-        document.styleSheets[0].insertRule(".notification-error { background-color: "+this.colorError+"; opacity: "+this.opacity+"; }", 1);
-        document.styleSheets[0].insertRule(".notification-confirm { background-color: "+this.colorConfirm+"; opacity: "+this.opacity+"; }", 1);
-        document.styleSheets[0].insertRule(".notification-warning { background-color: "+this.colorWarning+"; opacity: "+this.opacity+"; }", 1);
-        document.styleSheets[0].insertRule(".notification-info { background-color: "+this.colorInfo+"; opacity: "+this.opacity+"; }", 1);
-        
-        document.styleSheets[0].insertRule(".notification-title { margin: 0; font-size: 20px; font-weight: bold; }", 1);
-        document.styleSheets[0].insertRule(".notification-body { font-size: 13px; }", 1);
+
+        this.style.insertRule("."+this.owner+"-notification-item { position: inherit; width: "+this.notifWidth+"px; height: "+this.notifHeight+"px; "+cssItemPos+" overflow: hidden; cursor: pointer; transition: transform 0.25s ease-out, right 0.25s ease-out, left 0.25s ease-out, top 0.25s ease-out, bottom 0.25s ease-out; }", 0);
+
+        this.style.insertRule(".notification-content { padding: 15px 0 0 15px; pointer-events: none; }", 0);
+
+        this.style.insertRule(".notification-error { background-color: "+this.colorError+"; opacity: "+this.opacity+"; }", 0);
+        this.style.insertRule(".notification-success { background-color: "+this.colorSuccess+"; opacity: "+this.opacity+"; }", 0);
+        this.style.insertRule(".notification-warning { background-color: "+this.colorWarning+"; opacity: "+this.opacity+"; }", 0);
+        this.style.insertRule(".notification-info { background-color: "+this.colorInfo+"; opacity: "+this.opacity+"; }", 0);
+
+        this.style.insertRule(".notification-title { margin: 0; font-size: 20px; font-weight: bold; }", 0);
+        this.style.insertRule(".notification-body { font-size: 13px; }", 0);
     }
-    
+
     create(notifType, data) {
         var p = this;
-        
+
         var parser = new DOMParser();
         this.notifHTML = parser.parseFromString(this.notificationHTML.strcast(data), "text/html");
         this.notifHTML = this.notifHTML.childNodes[0].childNodes[1].childNodes[0];
         this.notifHTML.classList.add("notification-"+notifType);
-        
+
         return this.notifHTML;
     }
-    
+
     pushForward(pos, itemIndex, itemCnt, margin, padding) {
         var itemPos = (itemCnt - itemIndex)-1;
         var newStyle = ((padding + (itemPos*this.notifHeight) + (itemPos*margin))+margin+this.notifHeight)+"px";
-        
+
         if(pos=='bottom') {
             this.notifHTML.style.bottom = newStyle;
         } else if (pos=='top') {
             this.notifHTML.style.top = newStyle;
         }
     }
-    
+
     pushBackward(pos, itemIndex, itemCnt, margin, padding) {
         var itemPos = (itemCnt - itemIndex);
         var newStyle = (padding + (itemPos*this.notifHeight) + (itemPos*margin))-(margin+this.notifHeight)+"px";
-        
+
         if(pos=='bottom') {
             this.notifHTML.style.bottom = newStyle;
         } else if (pos=='top') {
             this.notifHTML.style.top = newStyle;
         }
     }
-    
+
     destroy(pos) {
         switch(pos) {
             case 'topright':
